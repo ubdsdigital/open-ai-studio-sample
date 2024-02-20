53233794e6b48b525ae34a9cbb059355479e61cf
Nathan Keegan <nathan.keegan@ubds.com>
2024-02-20 16:49:18 +0000

    fix: Undo previous change + change auth_update.sh to use CLIENT_ID

--- scripts/auth_update.py
+++ scripts/auth_update.py
@@ -21,25 +21,7 @@ def update_redirect_uris(credential, app_id, uri):
             }
         },
     )
-### Adding a new update_redirect_uri function so we can use the client_id to add the redirect uri
-    
-def test_update_redirect_uris(credential, client_id, uri):
-    urllib3.request(
-        "PATCH",
-        f"https://graph.microsoft.com/v1.0/applications/{client_id}",
-        headers={
-            "Authorization": "Bearer "
-            + credential.get_token("https://graph.microsoft.com/.default").token,
-        },
-        json={
-            "web": {
-                "redirectUris": [
-                    "http://localhost:5000/.auth/login/aad/callback",
-                    f"{uri}/.auth/login/aad/callback",
-                ]
-            }
-        },
-    )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -56,12 +38,6 @@ if __name__ == "__main__":
         required=False,
         help="Required. URI of the deployed application.",
     )
-    # Add client_id as an argument
-    parser.add_argument(
-        "--clientid",
-        required=False,
-        help="Required. URI of the deployed application.",
-    )
     args = parser.parse_args()
 
     credential = AzureDeveloperCliCredential()
@@ -70,10 +46,3 @@ if __name__ == "__main__":
         f"Updating application registration {args.appid} with redirect URI for {args.uri}"
     )
     # update_redirect_uris(credential, args.appid, args.uri)
-
-# Add new print statement to show redirect uri is being added to app registration (via the clientid)
-    print(
-        f"Updating application registration {args.clientid} with redirect URI for {args.uri}"
-    )
-    # Call the test redirect uri
-    test_update_redirect_uris(credential, args.client_id, args.uri)
