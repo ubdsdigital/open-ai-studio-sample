d3f829ca04ca1616f1da84940b8650b2dbad28e1
Nathan Keegan <nathan.keegan@ubds.com>
2024-02-27 14:09:11 +0000

    fix: Delete redundant test files

--- scripts/nathan-scripts-broken/newindex.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/sh
-
-. ./scripts/loadenv.sh
-
-echo 'Running "newindex.py"'
-./.venv/bin/python ./scripts/nathan-scripts/newindex.py --searchservice "$AZURE_SEARCH_SERVICE" --index "$AZURE_SEARCH_INDEX" --tenantid "$AZURE_TENANT_ID" --embeddingendpoint "$AZURE_OPENAI_EMBEDDING_ENDPOINT"
